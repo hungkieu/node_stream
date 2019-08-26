aa5db1f1643608cea0dd073c16b8754dce425d46
hungkq-1724 <kieu.quoc.hung@sun-asterisk.com>
2019-08-26 15:58:25 +0700

    #init

---
+++ chat-node.js
@@ -0,0 +1,19 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+const port = 3000;
+
+log = console.log;
+
+io.on('connection', (socket) => {
+  log('connected');
+  socket.on('message', (evt) => {
+    log(evt);
+    socket.broadcast.emit('message', evt)
+  });
+})
+
+io.on('disconnect', (evt) => {
+  log('disconnected');
+});
+
+http.listen(port, () => log(`server listening on port: ${port}`));
